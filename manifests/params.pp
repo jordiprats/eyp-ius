635783770e4b73621bd051f7cfe0b42f41469f02
Jordi Prats <jprats@systemadmin.es>
2017-01-25 10:48:01 +0100

    metadata lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,27 +15,6 @@ class ius::params {
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
     }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            /^16.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
-      }
-    }
     default: { fail('Unsupported OS!')  }
   }
 }
