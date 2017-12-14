313c5077288a4d8e9ffd0ce0ff598fad2cfba165
Jordi Prats <jprats@systemadmin.es>
2017-12-14 14:51:37 +0100

    target repo

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,14 +6,17 @@ class ius::config inherits ius {
 
   yumrepo { 'ius':
     enabled => $ius::enable_repo_ius,
+    target  => '/etc/yum.repos.d/ius.repo',
   }
 
   yumrepo { 'ius-debuginfo':
     enabled => $ius::enable_repo_ius_debug,
+    target  => '/etc/yum.repos.d/ius.repo',
   }
 
   yumrepo { 'ius-source':
     enabled => $ius::enable_repo_ius_source,
+    target  => '/etc/yum.repos.d/ius.repo',
   }
 
   #
@@ -22,14 +25,17 @@ class ius::config inherits ius {
 
   yumrepo { 'ius-dev':
     enabled => $ius::enable_repo_ius_dev,
+    target  => '/etc/yum.repos.d/ius-dev.repo',
   }
 
   yumrepo { 'ius-dev-debuginfo':
     enabled => $ius::enable_repo_ius_dev_debug,
+    target  => '/etc/yum.repos.d/ius-dev.repo',
   }
 
   yumrepo { 'ius-dev-source':
     enabled => $ius::enable_repo_ius_dev_source,
+    target  => '/etc/yum.repos.d/ius-dev.repo',
   }
 
   #
@@ -38,14 +44,17 @@ class ius::config inherits ius {
 
   yumrepo { 'ius-testing':
     enabled => $ius::enable_repo_ius_testing,
+    target  => '/etc/yum.repos.d/ius-testing.repo',
   }
 
   yumrepo { 'ius-testing-debuginfo':
     enabled => $ius::enable_repo_ius_testing_debug,
+    target  => '/etc/yum.repos.d/ius-testing.repo',
   }
 
   yumrepo { 'ius-testing-source':
     enabled => $ius::enable_repo_ius_testing_source,
+    target  => '/etc/yum.repos.d/ius-testing.repo',
   }
 
 }
